307a0609ff6f9d4f38af2006f7b4255849c05a0e
Daniel <danielrs9504@gmail.com>
2024-10-02 20:28:57 -0500

    add views for books

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,23 +1,50 @@
 class BooksController < ApplicationController
+  before_action :set_book, only: %i[show edit update destroy]
+
   def index
     @books = Book.all
   end
 
   def show
+    @comment = Comment.new
   end
 
   def new
+    @book = Book.new
   end
 
   def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to @book, notice: 'El libro fue creado exitosamente.'
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+    if @book.update(book_params)
+      redirect_to @book, notice: 'El libro fue actualizado exitosamente.'
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @book.destroy
+    redirect_to books_url, notice: 'El libro fue eliminado exitosamente.'
+  end
+
+  private
+
+  def set_book
+    @book = Book.find(params[:id])
+  end
+
+  def book_params
+    params.require(:book).permit(:title, :author, :description)
   end
 end
