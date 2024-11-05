07977bb4a4e0ce68a5471144aa4167b0834902bb
Daniel <danielrs9504@gmail.com>
2024-11-05 09:22:41 -0500

    Add posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,7 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: %i[show edit update destroy]
+  before_action :require_admin, except: %i[index show]
+
   def index
     @posts = Post.all
   end
@@ -7,17 +10,46 @@ class PostsController < ApplicationController
   end
 
   def new
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to @post, notice: 'Post creado exitosamente'
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+    if @post.update(post_params)
+      redirect_to @post, notice: 'Post actualizado exitosamente'
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @post = Post.find(params[:id])
+
+    Post.transaction do
+      @post.destroy
+    end
+
+    redirect_to posts_path, notice: 'El post ha sido eliminado exitosamente.'
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :content)
   end
 end
